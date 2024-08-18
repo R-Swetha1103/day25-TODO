2da526acaf4098285d3ddd9a238e9022ed683878
R-Swetha1103 <swetharagupathy1103@gmail.com>
2024-08-18 19:11:47 +0530

    react 25

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,34 @@
 
-import React from 'react'
-import Card from './card.jsx'
+import React, { useState } from 'react'
+import TopBar from './components/TopBar'
 function App() {
-  let data= [
+  let [todo,setTodo]=useState([
     {
-      plan:"FREE",
-      price:0,
-      isUser:true,
-      user:"Single User",
-      isStorage:true,
-      storage:"5 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:false,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:false,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:false,
-      subDomain:"Free Subdomain",
-      isReports:false,
-      reports:"Monthly Status Reports"
-    },
-    {
-      plan:"PLUS",
-      price:9,
-      isUser:true,
-      user:"5 Users",
-      isStorage:true,
-      storage:"50 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:true,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:true,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:true,
-      subDomain:"Free Subdomain",
-      isReports:false,
-      reports:"Monthly Status Reports"
-    },
-    {
-      plan:"PRO",
-      price:49,
-      isUser:true,
-      user:"Unlimited Users",
-      isStorage:true,
-      storage:"150 GB",
-      isPublicProjects:true,
-      publicProjects:"Unlimited Public Projects",
-      isCommuntityAccess:true,
-      communtityAccess:"Community Access",
-      isPrivateProjects:true,
-      privateProjects:"Unlimited Private Projects",
-      isPhoneSupport:true,
-      phoneSupport:"Dedicated Phone Support",
-      isSubDomain:true,
-      subDomain:"Free Subdomain",
-      isReports:true,
-      reports:"Monthly Status Reports"
-    }
-  ]
-  return <section className="pricing py-5">
-  <div className="container">
-    <div className="row">
-      {
-        data.map((e,i)=>{
-          return  <Card data={e} key={i} />
-        })
-      }
-     
-      
-      
-    </div>
-  </div>
-</section>
+    id:1,
+    title:" Office Task-1",
+    description:"this is the description for My first Task",
+    status:false
+
+  },
+  {
+    id:2,
+    title:" Office Task-2",
+    description:"this is the description for My second Task",
+    status:true
+    
+  },
+  {
+    id:3,
+    title:" Office Task-3",
+    description:"this is the description for My third Task",
+    status:false
+    
+  }
+])
+let [completed,setCompleted]=useState("All")
+  return <>
+  <TopBar todo={todo} setTodo={setTodo} completed={completed} setCompleted={setCompleted}/>
+  </>
 }
 
 export default App
